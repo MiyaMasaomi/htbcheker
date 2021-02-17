6907c282259cea27cf1bb36baf855611917206ab
MiyaMasaomi <masaomi0223@pandapharmacist.com>
2021-02-17 14:18:21 +0900

    エラーメッセージと日本語導入

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,16 +1,19 @@
 class DiariesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
-    @diaries = Diary.all
-    @sleep_average = Diary.average(:sleep)
-    @meal_average = Diary.average(:meal)
-    @motion_average = Diary.average(:motion)
-    @study_average = Diary.average(:study)
-    @output_average = Diary.average(:output)
-    @today_average = Diary.average(:today_goal)
-    @margin_average = Diary.average(:margin)
-    @tired_average = Diary.average(:tired)
-    @refresh_average = Diary.average(:refresh)
+    if user_signed_in?
+      @diaries = Diary.where(user_id: current_user.id)
+      @sleep_average = @diaries.average(:sleep)
+      @meal_average = @diaries.average(:meal)
+      @motion_average = @diaries.average(:motion)
+      @study_average = @diaries.average(:study)
+      @output_average = @diaries.average(:output)
+      @today_average = @diaries.average(:today_goal)
+      @margin_average = @diaries.average(:margin)
+      @tired_average = @diaries.average(:tired)
+      @refresh_average = @diaries.average(:refresh)
+    end
   end
 
   def new
@@ -22,7 +25,6 @@ class DiariesController < ApplicationController
     @body = @diary.sleep.length + @diary.meal.length + @diary.motion.length
     @technique = @diary.study.length + @diary.output.length + @diary.today_goal.length
     @heart = @diary.margin.length + @diary.tired.length + @diary.refresh.length
-    @htb = @body + @technique + @heart
   end
 
   def create
